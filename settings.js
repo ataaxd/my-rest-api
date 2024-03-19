66e8f008057a4b159dc6f8f16734221282dd1d4e
ataaxd <151380638+ataaxd@users.noreply.github.com>
2024-03-20 03:12:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'AtaaXD'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://ataahackofficial:wkwk0987@cluster0.yn2k0lv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "ataahackofficial@gmail.com" //email
 global.email_password = "" //application password email
