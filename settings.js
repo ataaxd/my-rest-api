a3abdea2e89d0cfe36e845783543bab6af5e7988
ataaxd <151380638+ataaxd@users.noreply.github.com>
2024-03-20 08:46:36 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ global.creator = 'AtaaXD'// yourname
 global.MONGO_DB_URI = "mongodb+srv://ataa666ofc:uW1VWFOUTxfSIITi@cluster0.uxuuhz6.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "ataahackofficial@gmail.com" //email
-global.email_password = "nfdl yppm moxk ohut" //application password email
+global.email_password = "nfdlyppmmoxkohut" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
